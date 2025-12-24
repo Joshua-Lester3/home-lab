0de4261c5096c0c8d14b1cad93b5a4d71178dce0
Joshua Lester <jlester3@ewu.edu>
2025-12-24 06:54:42 +0000

    remove sudo

--- scripts/enable-networking.sh
+++ scripts/enable-networking.sh
@@ -2,6 +2,6 @@
 
 # pretty sure this is bad practice, so find ways to automate this, like LXC container or something :)
 
-echo 'net.ipv4.ip_forward = 1' | sudo tee -a /etc/sysctl.d/99-tailscale.conf
-echo 'net.ipv6.conf.all.forwarding = 1' | sudo tee -a /etc/sysctl.d/99-tailscale.conf
-sudo sysctl -p /etc/sysctl.d/99-tailscale.conf
+echo 'net.ipv4.ip_forward = 1' | tee -a /etc/sysctl.d/99-tailscale.conf
+echo 'net.ipv6.conf.all.forwarding = 1' | tee -a /etc/sysctl.d/99-tailscale.conf
+sysctl -p /etc/sysctl.d/99-tailscale.conf
